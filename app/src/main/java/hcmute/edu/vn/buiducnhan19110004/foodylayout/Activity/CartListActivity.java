2b7dfc3cdfcad255cda1bad8f6d64666237ed06c
Bui Duc Nhan <Bui Duc Nhan@DESKTOP-OB2KPUE>
2022-04-18 17:06:22 +0700

    [init]

---
+++ app/src/main/java/hcmute/edu/vn/buiducnhan19110004/foodylayout/Activity/CartListActivity.java
@@ -0,0 +1,111 @@
+package hcmute.edu.vn.buiducnhan19110004.foodylayout.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import hcmute.edu.vn.buiducnhan19110004.foodylayout.Adaptor.CartListAdapter;
+import hcmute.edu.vn.buiducnhan19110004.foodylayout.Helper.ManagementCart;
+import hcmute.edu.vn.buiducnhan19110004.foodylayout.Interface.ChangeNumberItemsListener;
+import hcmute.edu.vn.buiducnhan19110004.foodylayout.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class CartListActivity extends AppCompatActivity {
+    private RecyclerView.Adapter adapter;
+    private RecyclerView recyclerViewList;
+    private ManagementCart managementCart;
+    TextView totalFeeTxt, taxTxt, deliveryTxt, totalTxt, emptyTxt;
+    private double tax;
+    private ScrollView scrollView;
+    private TextView textViewCheckOut;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_list);
+
+        managementCart = new ManagementCart(this);
+
+        initView();
+        initList();
+        CalculateCart();
+        bottomNavigation();
+    }
+
+    private void bottomNavigation() {
+        FloatingActionButton floatingActionButton = findViewById(R.id.cartBtn);
+        LinearLayout homeBtn = findViewById(R.id.homeBtn);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CartListActivity.this, CartListActivity.class));
+            }
+        });
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CartListActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+    private void initView() {
+        recyclerViewList = findViewById(R.id.recyclerViewCategories);
+        totalFeeTxt = findViewById(R.id.totalFeeTxt);
+        taxTxt = findViewById(R.id.taxTxt);
+        deliveryTxt = findViewById(R.id.deliveryTxt);
+        totalTxt = findViewById(R.id.totalTxt);
+        emptyTxt = findViewById(R.id.emptyTxt);
+        scrollView = findViewById(R.id.scrollViewMain);
+        recyclerViewList=findViewById(R.id.cartViewCartList);
+        textViewCheckOut = findViewById(R.id.textViewCheckOut);
+        textViewCheckOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CartListActivity.this, CheckOutActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initList() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerViewList.setLayoutManager(linearLayoutManager);
+        adapter = new CartListAdapter(managementCart.getListCart(), this, new ChangeNumberItemsListener() {
+            @Override
+            public void changed() {
+                CalculateCart();
+            }
+        });
+
+        recyclerViewList.setAdapter(adapter);
+        if (managementCart.getListCart().isEmpty()) {
+            emptyTxt.setVisibility(View.VISIBLE);
+            scrollView.setVisibility(View.GONE);
+        } else {
+            emptyTxt.setVisibility(View.GONE);
+            scrollView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void CalculateCart() {
+        double percentTax = 0.02;
+        double delivery = 10;
+
+        tax = Math.round((managementCart.getTotalFee() * percentTax) * 100) / 100;
+        double total = Math.round((managementCart.getTotalFee() + tax + delivery) * 100) / 100;
+        double itemTotal = Math.round(managementCart.getTotalFee() * 100) / 100;
+
+        totalFeeTxt.setText("$" + itemTotal);
+        taxTxt.setText("$" + tax);
+        deliveryTxt.setText("$" + delivery);
+        totalTxt.setText("$" + total);
+    }
+}
---
+++ app/src/main/java/hcmute/edu/vn/buiducnhan19110004/foodylayout/Activity/CheckOutActivity.java
@@ -0,0 +1,17 @@
+package hcmute.edu.vn.buiducnhan19110004.foodylayout.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import hcmute.edu.vn.buiducnhan19110004.foodylayout.R;
+
+public class CheckOutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+    }
+}
