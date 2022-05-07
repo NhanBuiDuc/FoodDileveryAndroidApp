d6a242b363b091bb098a3dd7af0dce0559734482
giangtiny <giangtiny12@gmail.com>
2022-05-08 01:03:47 +0700

    Add profile activity

---
+++ app/src/main/java/hcmute/edu/vn/buiducnhan19110004/foodylayout/Activity/ProfileActivity.java
@@ -0,0 +1,16 @@
+package hcmute.edu.vn.buiducnhan19110004.foodylayout.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import hcmute.edu.vn.buiducnhan19110004.foodylayout.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
